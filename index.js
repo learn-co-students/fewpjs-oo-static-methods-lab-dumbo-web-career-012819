969f92b29ed464a9eb4afa15ba7e8cf08c793acd
Krystle Justine <KrystleJustine@gmail.com>
2019-03-22 10:11:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+ static capitalize(name){
+     return name.charAt(0).toUpperCase() + name.slice(1);
+ }
+
+ static sanitize (string){
+ return string.replace(/[^A-Za-z0-9-" ']+/g, '');
+ }
+
+ static titleize(string) {
+    const exclude = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    let arr = string.split(' ');
+
+    let title = arr.map(word => {
+        if(word === arr[0]) {
+            return this.capitalize(word);
+        }
+        else if(exclude.includes(word)) {
+            return word;
+        } else {
+            return this.capitalize(word);
+        }
+    });
+    return title.join(' ');
+     }
+
 }
+  //add static methods here
